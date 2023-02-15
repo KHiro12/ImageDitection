e97806a0ba95331a324c6a7a687ddc5c085e5b63
KHiro12 <0hf095277264f6e@ezweb.ne.jp>
2023-02-15 21:08:36 +0900

    移動平均フィルタ追加

--- package/Filtering.py
+++ package/Filtering.py
@@ -1,5 +1,19 @@
+import cv2
+
+
 # フィルタリングメイン関数
 def FilteringMain(img,  use_filter_val):
     out_img = img.copy()
 
+    if use_filter_val & 1:
+        new_img = out_img.copy()
+
+        # 移動平均フィルタ
+        out_img = AveragingFiltering(new_img)
+
     return out_img
+
+
+# 移動平均フィルタ
+def AveragingFiltering(img):
+    return cv2.blur(img, (5, 5))
