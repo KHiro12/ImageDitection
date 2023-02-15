42bd1d42afeab2781f593533358a85226638c0fa
KHiro12 <0hf095277264f6e@ezweb.ne.jp>
2023-02-15 18:59:56 +0900

    設定ファイルで数値を設定するように変更

--- ImageDitection.py
+++ ImageDitection.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import cv2
@@ -20,9 +21,28 @@ def OutPutMessage():
     message =   u'''
 ■操作説明
 Enter：終了
-1：フィルタリング再設定
                 '''
     print(message)
+
+
+# 設定ファイルを読み込む
+def ReadSetting(setting_target):
+    output_dict = dict(zip(setting_target, [0]*len(setting_target)))
+    update_flg = False
+
+    if os.path.exists('.\\setting.json'):
+        with open('.\\setting.json') as f:
+            df = json.load(f)
+
+            if len(output_dict) == len(df):
+                output_dict = df
+                update_flg = True
+
+    if not update_flg:
+        with open('.\\setting.json', 'w') as f:
+            json.dump(output_dict, f, indent=2)
+    
+    return output_dict
 
 
 # 画像処理
@@ -36,15 +56,18 @@ def ImageProcessing(file_path):
 
         cap_file = cv2.VideoCapture(file_path)
 
-        update_module_cnt = 1
-        update_flg = [True] * update_module_cnt
+        setting_target = [  'Filtering'
+                            ]
+
+        # 設定読み込み
+        setting_dict = ReadSetting(setting_target)
         
         while True:
             __, frame = cap_file.read()
             cv2.imshow("BaseImg", frame)
 
             # ①フィルタリング
-            img_filter = FilteringMain(frame, update_flg[0])
+            img_filter = FilteringMain(frame, setting_dict[setting_target[0]])
             cv2.imshow("Filtering", img_filter)
 
             key = cv2.waitKey(30)
@@ -52,10 +75,6 @@ def ImageProcessing(file_path):
             # 閉じるようにしてみる
             if key == 13:
                 break
-            elif key == 49:
-                update_flg[0] = True
-            
-            update_flg = [False] * update_module_cnt
 
 
 if __name__ == '__main__':
--- package/Filtering.py
+++ package/Filtering.py
@@ -1,25 +1,5 @@
-Use_Filter_Val = 0  # フィルタリングを行う判定用の数値
-
-
 # フィルタリングメイン関数
-def FilteringMain(img, change_flg):
-    global Use_Filter_Val
-
+def FilteringMain(img,  use_filter_val):
     out_img = img.copy()
-    filter_val = ChangeMode(change_flg, Use_Filter_Val)
-
-    Use_Filter_Val = filter_val
 
     return out_img
-
-
-# フィルタリング設定
-def ChangeMode(change_flg, before_val):
-    output_val = before_val
-
-    if change_flg:
-        message = '''
-フィルタリング
-'''
-        output_val = input(message)
-    return output_val
